3b3731b6fd7dc96a2c561541708382d366ddafcd
savvy chezhian <savvychez@gmail.com>
2023-05-13 00:46:56 -0500

    🙈 thanks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,7 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended","next"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -14,9 +14,7 @@ export default function SignIn({ providers }: InferGetServerSidePropsType<typeof
                   <h1 className="bg-acid-white pb-4 "><strong>5a | </strong>login with your preferred provider</h1>
                   {Object.values(providers).map((provider) => (
                       <div key={provider.name}>
-                      <button className="bg-acid-green w-full my-2 py-3 rounded-3xl border-2 border-acid-black hover:bg-acid-darkened-green " onClick={
-                        // eslint-disable-next-line @typescript-eslint/no-misused-promises 
-                        async () => signIn(provider.id).then(() => {console.log("success!")}).catch((err) => {console.error(err)})}>
+                      <button className="bg-acid-green w-full my-2 py-3 rounded-3xl border-2 border-acid-black hover:bg-acid-darkened-green " onClick={() => signIn(provider.id)}>
                           sign in with {provider.name.toLocaleLowerCase()}
                       </button>
                       </div>
